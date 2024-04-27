79ea13f7cd9d197ce3be36fa636484a8b95e6811
jewelcodes <omarmelghoul01@gmail.com>
2024-04-26 20:17:25 -0400

    home: second section

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -3,11 +3,16 @@ import './Home.css';
 
 function Home() {
     return (
-        <section className="home-banner">
-            <h1>Yearning?<br />Know what you wanna hear?</h1>
-            <div className="phone">
-            </div>
-        </section>
+        <>
+            <section className="home-banner">
+                <h1>Yearning?<br />Know what you wanna hear?</h1>
+                <div className="phone"></div>
+            </section>
+            <section className="home-description">
+                <div className="type"></div>
+                <h1>The Yearning Vault was created for those of us who yearn for closure.</h1>
+            </section>
+        </>
     );
 }
 
