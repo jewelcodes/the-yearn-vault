fa9dfa96f0500ae5462911f2ec0851b925a74bf3
jewelcodes <omarmelghoul01@gmail.com>
2024-04-27 22:07:06 -0400

    submit: message color picker

--- frontend/src/components/MessageEditor.tsx
+++ frontend/src/components/MessageEditor.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Message.css";
 import "./MessageEditor.css";
 
 export default function MessageEditor(props:any) {
     let c = "message message-preview ";
-    let color = "message-blue";
+    
+    let [ color, setColor ] = useState("message-blue");
 
     const preventLineBreak = (e:any) => {
         //console.log(e);
@@ -16,6 +17,10 @@ export default function MessageEditor(props:any) {
         return true;
     };
 
+    useEffect(() => {
+
+    }, [])
+
     return (
         <>
             <input placeholder="Who do you need to hear it from?" type="text" className="message-sender-input" />
@@ -24,7 +29,11 @@ export default function MessageEditor(props:any) {
             </div>
             
             <div className="color-picker">
-                <div></div>
+                <div className="color-blue" onClick={() => {setColor("message-blue")}}></div>
+                <div className="color-red" onClick={() => {setColor("message-red")}}></div>
+                <div className="color-purple" onClick={() => {setColor("message-purple")}}></div>
+                <div className="color-orange" onClick={() => {setColor("message-orange")}}></div>
+                <div className="color-green" onClick={() => {setColor("message-green")}}></div>
             </div>
         </>
     );
