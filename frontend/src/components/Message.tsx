e9d7f8a7b46a2bd12632c658e709e17e7de0eefb
jewelcodes <omarmelghoul01@gmail.com>
2024-04-28 19:48:51 -0400

    message: fix styling

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -2,13 +2,8 @@ import React from "react";
 import "./Message.css";
 
 export default function Message(props:any) {
-    let c = "message ";
+    let c = "message " + props.color;
     if(props.preview) c += "message-preview ";
-    if(props.color === "blue") c += "message-blue";
-    if(props.color === "purple") c += "message-purple";
-    if(props.color === "red") c += "message-red";
-    if(props.color === "green") c += "message-green";
-    if(props.color === "orange") c += "message-orange";
 
     return (
         <div className={c}>
