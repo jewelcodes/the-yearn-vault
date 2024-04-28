0e27a8561da96edff36ab2be4098b94766e1ec5c
jewelcodes <omarmelghoul01@gmail.com>
2024-04-27 22:08:19 -0400

    submit: remove redundant page title

--- frontend/src/pages/Submit/Submit.tsx
+++ frontend/src/pages/Submit/Submit.tsx
@@ -5,8 +5,6 @@ import "./Submit.css";
 export default function Submit() {
     return (
         <section className="sec">
-            <h1>What do you need to hear?</h1>
-            <div className="v-spacer" style={{height:"1.5em"}} />
             <form>
                 <MessageEditor color="blue" />
             </form>
