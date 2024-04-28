837d78a369cdb7f646da318334daabb429219192
jewelcodes <omarmelghoul01@gmail.com>
2024-04-27 22:19:36 -0400

    submit: styling fixes

--- frontend/src/pages/Submit/Submit.tsx
+++ frontend/src/pages/Submit/Submit.tsx
@@ -9,7 +9,7 @@ export default function Submit() {
             <form>
                 <MessageEditor color="blue" />
                 <p className="disclaimer"><strong>Disclaimer:</strong> By clicking send below, you acknowledge that you have read and agree to the <Link to="/privacy">privacy statement</Link>. You also agree to not abuse the platform by posting identifying content (i.e. usernames, phone numbers, etc.), spamming, or posting NSFW content.</p>
-                <input type="submit" value="Send" className="button button-default" />
+                <input type="submit" value="Send" className="button button-default submit" />
             </form>
         </section>
     );
