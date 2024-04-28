b64ee4cec4e3b3705b63adca81e0f9854e5c866e
jewelcodes <omarmelghoul01@gmail.com>
2024-04-27 22:28:09 -0400

    submit: change send to submit for consistency

--- frontend/src/pages/Submit/Submit.tsx
+++ frontend/src/pages/Submit/Submit.tsx
@@ -8,8 +8,8 @@ export default function Submit() {
         <section className="sec">
             <form>
                 <MessageEditor color="blue" />
-                <p className="disclaimer"><strong>Disclaimer:</strong> By clicking send below, you acknowledge that you have read and agree to the <Link to="/privacy">privacy statement</Link>. You also agree to not abuse the platform by posting identifying content (i.e. usernames, phone numbers, etc.), spamming, or posting NSFW content.</p>
-                <input type="submit" value="Send" className="button button-default submit" />
+                <p className="disclaimer"><strong>Disclaimer:</strong> By clicking submit below, you acknowledge that you have read and agree to the <Link to="/privacy">privacy statement</Link>. You also agree to not abuse the platform by posting identifying content (i.e. usernames, phone numbers, etc.), spamming, or posting NSFW content.</p>
+                <input type="submit" value="Submit" className="button button-default submit" />
             </form>
         </section>
     );
