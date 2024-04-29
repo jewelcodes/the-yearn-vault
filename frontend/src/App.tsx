fe2c62847fcd803313d5a7c443705ca028392479
jewelcodes <omarmelghoul01@gmail.com>
2024-04-29 19:06:52 -0400

    components: move scroll to top to components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ScrollToTop from "./ScrollToTop";
+import ScrollToTop from "./components/ScrollToTop";
 import Layout from "./pages/Layout/Layout";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
