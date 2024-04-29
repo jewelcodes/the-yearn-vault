239a6d92664c3d9209857cdf7755a393baa90924
jewelcodes <omarmelghoul01@gmail.com>
2024-04-29 10:43:07 -0400

    message editor: allow submitting anonymous entries

--- frontend/src/components/MessageEditor.tsx
+++ frontend/src/components/MessageEditor.tsx
@@ -20,7 +20,7 @@ export default function MessageEditor(props:any) {
     return (
         <>
             <input name="color" id="color" type="hidden" value={color} />
-            <input name="sender" id="sender" required minLength={2} maxLength={20} autoComplete="off" placeholder="Who do you need to hear it from?" type="text" className="message-sender-input" />
+            <input name="sender" id="sender" maxLength={20} autoComplete="off" placeholder="Optional: Who do you need to hear it from?" type="text" className="message-sender-input" />
             <div className={c+color}>
                 <textarea name="message" id="message" required minLength={8} maxLength={100} placeholder="What do you need to hear?" className="message-text" onKeyDown={preventLineBreak} />
             </div>
