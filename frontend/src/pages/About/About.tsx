d8f783906e0cda7566b943cde252883b5beb583a
jewelcodes <omarmelghoul01@gmail.com>
2024-04-28 23:24:38 -0400

    about: added link to source code

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -10,6 +10,7 @@ export default function About() {
             <p>Owing to <a href="https://theunsentproject.com/">the Unsent Project</a> as inspiration after submitting a message there for the first time, I thought of why not doing the exact opposite of that &ndash; that is, an archive of text messages you wish you had received rather than sent, but never did. I could very easily imagine myself submitting multiple entries if such a project existed, so I thought others might relate too. After a bit of searching, I did not find any such project, so decided to create it myself.</p>
             <p>That's really all this project is. From one yearner, to another.</p>
             <p>If you want to contact me personally, my up-to-date contact information is kept on <a href="https://jewelcodes.io/">my blog</a>. Consider <Link to="/support">buying me a coffee</Link> if you liked this.</p>
+            <p>The Yearn Vault, much like most of my other projects, is free and open-source software. The source code is available on <a href="https://github.com/jewelcodes/the-yearn-vault">GitHub</a>.</p>
         </section>
     )
 }
