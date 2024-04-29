5a170e564b425bc2b88130e3f6f0688040c3f6a5
jewelcodes <omarmelghoul01@gmail.com>
2024-04-28 23:07:47 -0400

    submissions: submissions page works

--- frontend/src/pages/Submissions/Submissions.tsx
+++ frontend/src/pages/Submissions/Submissions.tsx
@@ -1,18 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Submissions.css";
 import Message from "../../components/Message";
+import { get } from "../../wrapper/wrapper";
 
 export default function Submissions() {
+    let [page, setPage] = useState(0);
+    let [messages, setMessages] = useState([]);
+    let [loading, setLoading] = useState(false);
+
+    const loadPage = async () => {
+        if(loading) {
+            console.log("attempt to loadPage when already in progress");
+            return;
+        }
+
+        setLoading(true);
+
+        console.log("fetching page " + page);
+        // fetch from the API
+        let response = await get("search", String(page));
+        if(!response.ok) {
+            // TODO: ERROR HANDLING HERE
+        } else {
+            // fetch the messages one by one
+            console.log(response);
+
+            for(let i = 0; i < response.messages.length; i++) {
+                console.log("adding " + response.messages[i]);
+                let messageData = await get("read", response.messages[i]);
+                if(messageData.ok) {
+                    messages.push(
+                        <div key={response.messages[i]}>
+                            <Message color={messageData.color} text={messageData.message} sender={messageData.sender} />
+                        </div>
+                    );
+                }
+            }
+        }
+
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        if(!loading) loadPage();
+    }, [page]);
+
     return (
         <section className="sec">
             <h1>The Yearn Vault</h1>
             {/* TODO: implement a search function here */}
-            
+
             <div className="submissions-container">
-                <div>
-                    <Message color="message-blue" text="test message" sender="someone" />
-                </div>
+                {messages}
             </div>
         </section>
     );
--- frontend/src/wrapper/wrapper.ts
+++ frontend/src/wrapper/wrapper.ts
@@ -1,4 +1,4 @@
-const apiRoot = "http://127.0.0.1:8000/";   // TODO: fix when deploying
+const apiRoot = "http://127.0.0.1:3000/";   // TODO: fix when deploying
 
 export async function post(endpoint:string, body:string) {
     let req = await fetch(apiRoot + endpoint, {
@@ -11,5 +11,16 @@ export async function post(endpoint:string, body:string) {
 
     if(!req.ok || req.status != 200) return null;
 
+    return await req.json();
+}
+
+export async function get(endpoint:string, params:string) {
+    let req = await fetch(apiRoot + endpoint + "/" + params, {
+        method: "GET",
+        mode: "cors",
+        cache: "no-cache"
+    });
+
+    if(!req.ok || req.status != 200) return null;
     return await req.json();
 }
