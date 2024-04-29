f789e77b3d404f03af828a893f507dc994bcd409
jewelcodes <omarmelghoul01@gmail.com>
2024-04-29 13:21:59 -0400

    backend: remove unnecessary imports

--- backend/src/endpoints/read.ts
+++ backend/src/endpoints/read.ts
@@ -1,5 +1,4 @@
 import express, { Express, Request, Response } from "express";
-import { v4 as uuidv4 } from "uuid";
 import { messageSchema } from "../database";
 import mongoose from "mongoose";
 import respond from "../respond";
