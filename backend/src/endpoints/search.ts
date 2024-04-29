3ca8f6cf9d50eea6c667cb78cfdf461933234b5b
jewelcodes <omarmelghoul01@gmail.com>
2024-04-29 11:58:38 -0400

    backend: explicitly return page size for /search

--- backend/src/endpoints/search.ts
+++ backend/src/endpoints/search.ts
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 import respond from "../respond";
 
 export default async function search(req: Request, res: Response) {
+    const itemsPerPage:number = 30;     // constant
+
     let response:any = {};
     let messages:any = [];
 
@@ -13,14 +15,15 @@ export default async function search(req: Request, res: Response) {
 
     // 20 items per page is reasonable I think
     const Model = mongoose.model("message", messageSchema);
-    const pageCount:number = Math.floor((await Model.countDocuments() + 19) / 20);
-    const docs = await Model.find().skip(page*20).sort({$natural: -1}).limit(20);
+    const pageCount:number = Math.floor((await Model.countDocuments() + itemsPerPage - 1) / itemsPerPage);
+    const docs = await Model.find().skip(page*itemsPerPage).sort({$natural: -1}).limit(itemsPerPage);
 
     for(let i = 0; docs.length && i < docs.length; i++) {
         messages.push(docs[i].id);
     }
 
     response.ok = true;
+    response.pageSize = itemsPerPage;
     response.pages = pageCount;
     response.messages = messages;
     respond(res, response);
