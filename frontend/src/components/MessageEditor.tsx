6baa39939cdbf207331421c946ffdb18da89f045
jewelcodes <omarmelghoul01@gmail.com>
2024-04-29 10:53:34 -0400

    message editor: add black as a color choice

--- frontend/src/components/MessageEditor.tsx
+++ frontend/src/components/MessageEditor.tsx
@@ -20,17 +20,18 @@ export default function MessageEditor(props:any) {
     return (
         <>
             <input name="color" id="color" type="hidden" value={color} />
-            <input name="sender" id="sender" maxLength={20} autoComplete="off" placeholder="Optional: Who do you need to hear it from?" type="text" className="message-sender-input" />
+            <input name="sender" id="sender" maxLength={20} autoComplete="off" placeholder="Optional: From who?" type="text" className="message-sender-input" />
             <div className={c+color}>
-                <textarea name="message" id="message" required minLength={8} maxLength={100} placeholder="What do you need to hear?" className="message-text" onKeyDown={preventLineBreak} />
+                <textarea name="message" id="message" required minLength={5} maxLength={100} placeholder="What do you need to hear?" className="message-text" onKeyDown={preventLineBreak} />
             </div>
             
             <div className="color-picker">
-                <div className="color-blue" onClick={() => {setColor("message-blue")}}></div>
                 <div className="color-red" onClick={() => {setColor("message-red")}}></div>
+                <div className="color-green" onClick={() => {setColor("message-green")}}></div>
+                <div className="color-blue" onClick={() => {setColor("message-blue")}}></div>
                 <div className="color-purple" onClick={() => {setColor("message-purple")}}></div>
                 <div className="color-orange" onClick={() => {setColor("message-orange")}}></div>
-                <div className="color-green" onClick={() => {setColor("message-green")}}></div>
+                <div className="color-black" onClick={() => {setColor("message-black")}}></div>
             </div>
         </>
     );
